65f56e789e23c8f841c84122aeb8861efb33836b
lukaofirst <lucasmacedo123@gmail.com>
2022-10-21 21:44:21 -0300

    registering services in an IoC container

---
+++ IoC/DependencyContainer.cs
@@ -0,0 +1,43 @@
+﻿using Application.Interfaces;
+using Application.Mappings;
+using Application.Services;
+using Domain.Interfaces;
+using Infra.Data;
+using Infra.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IoC
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection StartServices(this IServiceCollection services, IConfiguration config)
+        {
+            // Database
+            services.AddDbContext<DataContext>(opt =>
+                opt.UseSqlServer(
+                    config.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly(typeof(DataContext).Assembly.FullName)
+                )
+            );
+
+            // Repositories
+            services.AddScoped<IPersonRepository, PersonRepository>();
+
+            // Services
+            services.AddScoped<IPersonService, PersonService>();
+
+            // AutoMapper
+            services.AddAutoMapper(typeof(DomainToDTO));
+            services.AddAutoMapper(typeof(DomainToCommand));
+
+            // Mediator
+            var myHandlers = AppDomain.CurrentDomain.Load("Application");
+            services.AddMediatR(myHandlers);
+
+            return services;
+        }
+    }
+}
