6fc999159ac43cd5321ab84ee42f5a6e699b2f96
lukaofirst <lucasmacedo123@gmail.com>
2022-10-21 21:40:06 -0300

    first commit and adding Domain's layer

---
+++ Domain/Interfaces/IPersonRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IPersonRepository
+    {
+        Task<List<Person>> GetAll();
+        Task<Person> GetById(int id);
+        Task<Person?> Insert(Person person);
+        Task<Person?> Update(Person person);
+        Task<bool> Delete(int id);
+    }
+}
