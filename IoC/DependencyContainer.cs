3ffd5070416412cff8f318d95826492922cb96e2
lukaofirst <lucasmacedo123@gmail.com>
2024-12-26 16:36:50 -0300

    updating project from .NET 6 to 9

--- IoC/DependencyContainer.cs
+++ IoC/DependencyContainer.cs
@@ -4,40 +4,39 @@ using Application.Services;
 using Domain.Interfaces;
 using Infra.Data;
 using Infra.Repositories;
-using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace IoC
 {
-    public static class DependencyContainer
-    {
-        public static IServiceCollection StartServices(this IServiceCollection services, IConfiguration config)
-        {
-            // Database
-            services.AddDbContext<DataContext>(opt =>
-                opt.UseSqlServer(
-                    config.GetConnectionString("DefaultConnection"),
-                    b => b.MigrationsAssembly(typeof(DataContext).Assembly.FullName)
-                )
-            );
+	public static class DependencyContainer
+	{
+		public static IServiceCollection StartServices(this IServiceCollection services, IConfiguration config)
+		{
+			// Database
+			services.AddDbContext<DataContext>(opt =>
+				opt.UseSqlServer(
+					config.GetConnectionString("DefaultConnection"),
+					b => b.MigrationsAssembly(typeof(DataContext).Assembly.FullName)
+				)
+			);
 
-            // Repositories
-            services.AddScoped<IPersonRepository, PersonRepository>();
+			// Repositories
+			services.AddScoped<IPersonRepository, PersonRepository>();
 
-            // Services
-            services.AddScoped<IPersonService, PersonService>();
+			// Services
+			services.AddScoped<IPersonService, PersonService>();
 
-            // AutoMapper
-            services.AddAutoMapper(typeof(DomainToDTO));
-            services.AddAutoMapper(typeof(DomainToCommand));
+			// AutoMapper
+			services.AddAutoMapper(typeof(DomainToDTO));
+			services.AddAutoMapper(typeof(DomainToCommand));
 
-            // Mediator
-            var myHandlers = AppDomain.CurrentDomain.Load("Application");
-            services.AddMediatR(myHandlers);
+			// Mediator
+			var myHandlers = AppDomain.CurrentDomain.Load("Application");
+			services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(myHandlers));
 
-            return services;
-        }
-    }
+			return services;
+		}
+	}
 }
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -4,19 +4,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.StartServices(builder.Configuration);
+builder.Services.AddOpenApi();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.MapOpenApi();
+	app.UseSwaggerUI(options =>
+	{
+		options.SwaggerEndpoint("/openapi/v1.json", "WebAPI v1");
+		options.RoutePrefix = string.Empty;
+	});
 }
 
 app.UseHttpsRedirection();
